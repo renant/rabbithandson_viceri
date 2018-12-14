84966649a52eabe9ca274dd861b33b3c20eb8d39
Renan Teixeira <renan_replay@live.com>
2018-12-14 21:11:21 -0200

    thread para consumir mensagens

--- ChatRoom/Program.cs
+++ ChatRoom/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Text;
+using System.Threading;
+using EasyNetQ;
+using EasyNetQ.Topology;
+using Newtonsoft.Json;
 
 namespace ChatRoom
 {
     class Program
     {
+        private static Guid _userId = Guid.NewGuid();
+        private static string _myQueueName;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _myQueueName = $"messages_{_userId}";
+
+            using (var bus = RabbitHutch.CreateBus("amqp://llyajxqz:D9Za_TFzDeYpWVsMXTd2yIH3rdhaGRlk@buffalo.rmq.cloudamqp.com/llyajxqz").Advanced)
+            {
+                var exchange = bus.ExchangeDeclare("my_exchange_fanout", ExchangeType.Fanout);
+                var myQueueMessages = bus.QueueDeclare(_myQueueName);
+                bus.Bind(exchange, myQueueMessages, "");
+
+                new Thread(() =>
+                {
+
+                    bus.Consume(myQueueMessages, (body, properties, info) =>
+                    {
+                        var response = Encoding.UTF8.GetString(body);
+                        var message = JsonConvert.DeserializeObject<Message>(response);
+                        //print message
+                    });
+
+                }).Start();
+            }
         }
     }
 }
