645b631f1abf08b9b735ab1c1997505e8690798f
Renan Teixeira <reteixeira@viceri.com.br>
2018-12-14 14:44:44 -0200

    implementando o publisher com exchange direct

--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text;
+using EasyNetQ;
+using EasyNetQ.Topology;
 
 namespace Publisher
 {
@@ -6,7 +9,20 @@ namespace Publisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var bus = RabbitHutch.CreateBus("amqp://llyajxqz:D9Za_TFzDeYpWVsMXTd2yIH3rdhaGRlk@buffalo.rmq.cloudamqp.com/llyajxqz").Advanced)
+            {
+                var exchange = bus.ExchangeDeclare("my_direct_exchange", ExchangeType.Direct);
+                var queue = bus.QueueDeclare("first_queue");
+                bus.Bind(exchange, queue, "first_queue");
+
+                while(true)
+                {
+                    Console.WriteLine("Send your message to consumers...");
+                    var msg = Console.ReadLine();
+                    var body = Encoding.UTF8.GetBytes(msg);
+                    bus.Publish(exchange, "first_queue", false, new MessageProperties(), body);
+                }
+            }
         }
     }
 }
